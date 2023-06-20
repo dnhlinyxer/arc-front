fe3295bd9601fbc723c282a3fc02bc956b43dc3a
dnhlinyxer360 <dnhlinyxer360@gmail.com>
2023-06-20 18:20:00 +0800

    first commit

---
+++ js/home.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+    // WOW.js
+    new WOW().init();
+
+    // navbar dropdown
+    $('.nav-dropdown').on('click', function (e) {
+        e.preventDefault();
+        $(this).siblings().slideToggle();
+        // $(this).parent().siblings().find('nav-dropdown-ul').slideUp();
+    });
+
+    // 手機板 展開/收合 導覽列
+    $('.hamburger-menu').on('click', function (e) {
+        e.preventDefault();
+        $('body').toggleClass('show-menu');
+    });
+
+    // swiper
+    // const swiper = new Swiper('.swiper', {
+    //     direction: 'horizontal',
+    //     loop: true,
+    //     speed: 3000,
+    //     autoplay: {
+    //         delay: 2000,
+    //     },
+    //     effect: 'fade',
+    //     pagination: {
+    //         el: '.swiper-pagination',
+    //         type: "bullets",
+    //         clickable: true,
+    //     },
+    // });
+
+    // owl-carousel
+    $(".owl-carousel").owlCarousel({
+        center: true,
+        loop:true,
+        margin: 5,
+        autoWidth:true,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        // items:5
+        // responsive:{
+        //     0:{
+        //         items:1
+        //     },
+        //     600:{
+        //         items:3
+        //     },
+        //     1000:{
+        //         items:5
+        //     }
+        // }
+    });
+
+    // totop
+    function showTotop() { 
+        // console.log($(this).scrollTop());
+        // 向下滾動300px才顯示
+        if ( $(this).scrollTop() > 300 ) {
+            $('.totop').fadeIn();
+        } else {
+            $('.totop').fadeOut();
+        }
+    }
+    $(window).scroll(showTotop);
+
+    $('.totop').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop:0
+        }, 1500);
+    });
+
+});
